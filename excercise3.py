c986ecd5c9ee06a10c443504a18f96719fa8d10e
henrymiessan <miessanhenri@gmail.com>
2025-07-17 09:24:57 +0000

    Decision making in programing

---
+++ excercise3.py
@@ -0,0 +1,18 @@
+
+# declare a variable that shoudn't not be grater than 100
+
+n= int(input("Enter a number :"))
+n<100
+
+if  90<= n <=100 :
+    print("Grade A")
+elif  80 <= n <=89:
+    print("Grade B")
+elif 70 <= n<=79:
+    print("Grade C")
+elif  0 <= n<70:
+    print ("Grade F ")
+else: print ("the number you typed is higher than 100 or below 0 check you number and type again ")
+
+
+
---
+++ excerise2.py
@@ -0,0 +1,11 @@
+# ask the user to enter a number 
+number= int(input("Enter a number:1"))
+
+# Decision making
+
+if number > 0 :
+    print(f"{number} is positive")
+
+elif number < 0 :
+    print(f"{number} is a negative ") 
+
